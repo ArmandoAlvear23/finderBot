c18ac840e9aade9d4b6b1fe1f9df6f773e661491
Armando Alvear <armandoalvear23@gmail.com>
2018-08-13 14:10:40 -0600

    test

--- bot_params.py
+++ bot_params.py
@@ -3,12 +3,12 @@ import bot_functions
 #Get login credentials
 login = bot_functions.bot_login()
 #Subreddit the bot will look inside of
-subreddit = ""
+subreddit = "nba"
 #Word the bot will look for
-word = ""
+word = "lebron"
 #Get saved comment IDs
 comment_id = bot_functions.get_saved_comments()
 #Number of comments to search
-limit = ""
+limit = "100"
 
 
